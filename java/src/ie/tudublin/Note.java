c3a51f4fa2e22ffea81e6a4853f4f6323b11dbe8
Ahmed Alazawy <aalazawy625@gmail.com>
2021-04-21 18:26:16 +0100

    commit 1

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,27 @@
+package ie.tudublin;
+
+public class Note {
+    private int duration;
+    private char note;
+}
+
+public int getDuration() {
+    returns duration;
+}
+
+public void setDuration(int duration) {
+    this.duration = duration;
+}
+
+public char getNote() {
+    return note;
+}
+
+public void setNote(char note) {
+    this.note = note;
+}
+
+public Note(int duration, char note) {
+    this.duration = duration;
+    this.note = note; 
+}
